c349773e7539c0133a860695e736a5a64355e713
Fabian Schober <fabian@dirtypaws.studio>
2020-12-04 15:51:32 +0100

    Fixed moved file for 2-1.js

--- 2-1.js
+++ 2-1.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 // could be done via readline, but requires more code to be transformed into an array =)
-const inputStr = fs.readFileSync("3-input.txt", "utf-8");
+const inputStr = fs.readFileSync("2-input.txt", "utf-8");
 const input = inputStr.split("\r\n"); // Watch out in Unix systems!
 
 const regex = /(?<min>\d+)-(?<max>\d+) (?<char>.): (?<pw>.+)$/;
