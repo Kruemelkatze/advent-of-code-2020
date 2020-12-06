b029d03fe4257644e1de4a9fdd7137d043d5d494
Kruemelkatze <schoberf@gmx.net>
2020-12-06 15:59:49 +0100

    6-1.js

---
+++ 6-1.js
@@ -0,0 +1,36 @@
+const TEST = false;
+
+const fs = require("fs");
+const { performance } = require('perf_hooks');
+
+const file = TEST ? "6-input-test.txt" : "6-input.txt";
+const inputStr = fs.readFileSync(file, "utf-8").replace(/\r/g, "")
+
+const t0 = performance.now();
+
+// The obvious solution would be to split groups and then simply iterate and count over them.
+// But I want to try something else :)
+// const groups = inputStr.split("\n\n");
+
+var totalSum = 0;
+var set = new Set();
+
+for (let i = 0; i < inputStr.length; i++) {
+    let c = inputStr.charAt(i);
+    if (c != '\n') {
+        if (!set.has(c)) {
+            totalSum++;
+            set.add(c);
+        }
+    } else if (inputStr.charAt(i + 1) == '\n') {
+        // next grp
+        console.log(totalSum);
+        set.clear();
+    }
+
+}
+
+const t1 = performance.now();
+
+console.log("Total Sum: " + totalSum);
+console.log(`Searching took ${t1 - t0}ms`)
