010548d6f3ec4cce3472719b59d6af2da8ed766d
Kruemelkatze <schoberf@gmx.net>
2020-12-16 00:20:02 +0100

    15-1 reworked

--- 15-1.js
+++ 15-1.js
@@ -3,43 +3,32 @@ const { performance } = require('perf_hooks');
 const t0 = performance.now();
 
 const startNumbers = [
-    [0, 3, 6],
-    [1, 3, 2],
-    [2, 1, 3],
-    [1, 2, 3],
-    [2, 3, 1],
-    [3, 2, 1],
-    [3, 1, 2],
+    // [0, 3, 6],
+    // [1, 3, 2],
+    // [2, 1, 3],
+    // [1, 2, 3],
+    // [2, 3, 1],
+    // [3, 2, 1],
+    // [3, 1, 2],
     [5, 2, 8, 16, 18, 0, 1],
 ];
 const results = startNumbers.map(playNumberGame);
 
 function playNumberGame(startNumbers) {
-    const lastSeen = {};
+    const lastSeen = new Map();
 
-    startNumbers.forEach((n, i) => lastSeen[n] = [i, -1]);
+    startNumbers.forEach((n, i) => lastSeen.set(n, i + 1));
     var last = startNumbers[startNumbers.length - 1];
 
-    for (let i = startNumbers.length; i < 2020; i++) {
-        let number;
-
-        let lastSeenBefore = lastSeen[last];
-        let wasFirstTime = lastSeenBefore[1] == -1;
-        if (wasFirstTime) {
-            number = 0;
-        } else {
-            number = lastSeenBefore[0] - lastSeenBefore[1];
-        }
-
-        let numberLastSeen = lastSeen[number];
-        if (numberLastSeen) {
-            numberLastSeen[1] = numberLastSeen[0];
-            numberLastSeen[0] = i;
-        } else {
-            lastSeen[number] = [i, -1];
-        }
-
+    let number = NaN;
+    for (let i = startNumbers.length; i < 30000000; i++) {
+        number = lastSeen.has(number) ? i - lastSeen.get(number) : 0;
+        lastSeen.set(last, i);
         last = number;
+
+        if (i % 1000000 === 0) {
+            console.log(i);
+        }
     }
 
     return last;
